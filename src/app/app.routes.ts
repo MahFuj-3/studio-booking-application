c6be45e425b843a18ff6a2eae6669c27b9828e58
MahFuj <pridesys.8416@gmail.com>
2025-03-21 20:38:30 +0600

    Modification

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,31 +1,21 @@
 import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './error-handlers/components/page-not-found/page-not-found.component';
-import { StudioListComponent } from './features/studio/components/studio-list/studio-list.component';
-import { BookingListComponent } from './features/booking/components/booking-list/booking-list.component';
 
 export const routes: Routes = [
-  // {
-  //   path: ':languageId',
-  //   children: [
-  //     {
-  //       path: '',
-  //       loadChildren: () =>
-  //         import('./features/features.routes').then((f) => f.featureRoutes),
-  //     },
-  //   ],
-  // },
+  {
+    path: ':languageId',
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./features/features.routes').then((f) => f.featureRoutes),
+      },
+    ],
+  },
   {
     path: '',
-    redirectTo: '/studio/list',
+    redirectTo: '/en/studio/list',
     pathMatch: 'full',
-  },
-  {
-    path: 'studio/list',
-    component: StudioListComponent,
-  },
-  {
-    path: 'booking/list',
-    component: BookingListComponent,
   },
   { path: '**', component: PageNotFoundComponent },
 ];
